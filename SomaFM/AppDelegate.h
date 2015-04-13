d2dbb6649907d84650b5f48363daab8faf03c2a5
Brian Wilson <brian@kinvey.com>
2015-04-13 11:42:45 -0400

    Initial Commit

---
+++ SomaFM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SomaFM
+//
+//  Created by Brian Wilson on 4/13/15.
+//  Copyright (c) 2015 Polytopes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SomaFM/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SomaFM
+//
+//  Created by Brian Wilson on 4/13/15.
+//  Copyright (c) 2015 Polytopes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
